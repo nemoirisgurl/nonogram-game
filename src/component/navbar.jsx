336c24197581712f3eab1fcdb307f88f09eb39c0
nemoirisgurl <noppanart5777@gmail.com>
2026-03-29 21:19:42 +0700

    fix logo

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -27,7 +27,7 @@ export default function Navbar() {
     <header style={topBarStyle}>
       <a href="#/" aria-label="Go to home" style={brandStyle}>
         <img
-          src="src/assets/logo.png"
+          src="/logo.png"
           alt="Nonogrammer logo"
           style={{ width: 36, height: 36, objectFit: "contain", flexShrink: 0 }}
         />
